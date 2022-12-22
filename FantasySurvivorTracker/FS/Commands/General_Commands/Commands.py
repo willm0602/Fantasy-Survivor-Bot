5abee45547cb15e0d48917e0d52f9c900a63fd31
willm62 <will.migdol@gmail.com>
2022-12-22 15:00:05 -0800

    fix file structure

--- FantasySurvivorTracker/FS/Commands/General_Commands/Commands.py
+++ FantasySurvivorTracker/FS/Commands/General_Commands/Commands.py
@@ -1,14 +1,14 @@
 
-
-from .FS.Commands.General_Commands.Leaderboard import LB_COMMAND
-from .FS.Commands.General_Commands.List_Survivors import LIST_SURVIVORS_COMMAND
-from .FS.Commands.General_Commands.checkLocked import CHECK_LOCKED_COMMAND
+from .Leaderboard import LB_COMMAND
+from .List_Survivors import LIST_SURVIVORS_COMMAND
+from .checkLocked import CHECK_LOCKED_COMMAND
 
 COMMANDS = [
     LIST_SURVIVORS_COMMAND,
     LB_COMMAND,
     CHECK_LOCKED_COMMAND
 ]
+
 COMMANDS.sort(
     key = lambda k: k.desc
 )
