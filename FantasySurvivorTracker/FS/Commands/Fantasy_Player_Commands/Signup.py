f75b1792a138ca42795884112d72a2d756ab0c3d
Will Migdol <will.migdol@gmail.com>
2023-09-30 11:01:50 -0700

    role sanity check

--- FantasySurvivorTracker/FS/Commands/Fantasy_Player_Commands/Signup.py
+++ FantasySurvivorTracker/FS/Commands/Fantasy_Player_Commands/Signup.py
@@ -11,10 +11,16 @@ from ...Command import Command
 from ...DB import DB
 
 
+
 class MockRole:
+    """We need to pass in an object w/ an id to simulate the role that we're 
+    modifying"""
+
     def __init__(self, id):
         self.id = int(id)
 
+    def __bool__(self):
+        return self.id is not None
 
 async def signup(msg: Message):
     user = msg.author
@@ -23,8 +29,10 @@ async def signup(msg: Message):
         db.create_fantasy_player(user)
         season_role = MockRole(db.get_setting("seasonRoleID"))
         not_playing_role = MockRole(db.get_setting('notSignedUpID'))
-        await user.add_roles(season_role)
-        await user.remove_roles(not_playing_role)
+        if season_role:
+            await user.add_roles(season_role)
+        if not not_playing_role:
+            await user.remove_roles(not_playing_role)
         await msg.channel.send("Succesfully signed up for the Fantasy League!")
     else:
         raise Exception("Error: You are already signed up!")
